18460ef8189042f39ad2e9f5f43d08611dc9afc3
Edward <edward.azarevich@gmail.com>
2026-03-24 17:39:35 +0300

    add new Suspense for LatestInvoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,10 @@ import { Suspense } from "react";
 
 import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/skeletons";
 
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
@@ -35,7 +38,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
