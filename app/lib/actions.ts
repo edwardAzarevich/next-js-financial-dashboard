6165b41e36112887da9d912ab2df1bceb4e13b41
Edward <edward.azarevich@gmail.com>
2026-04-16 15:00:45 +0300

    add update form after add new invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import postgres from "postgres";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
-
-import { z } from "zod";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -29,4 +30,7 @@ export async function createInvoice(formData: FormData) {
      INSERT INTO invoices (customer_id, amount, status, date)
      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
    `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
