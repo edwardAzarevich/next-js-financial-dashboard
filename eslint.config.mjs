89c34a37bddfafa8f90d4fd20a181ffa8240dc10
Edward <edward.azarevich@gmail.com>
2026-05-04 20:39:26 +0300

    add eslint in project

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import nextConfig from 'eslint-config-next';
+
+const eslintConfig = [
+    ...nextConfig,
+    {
+        ignores: [
+            '.next/**',
+            'out/**',
+            'build/**',
+            'next-env.d.ts',
+            'node_modules/**',
+        ],
+    },
+];
+
+export default eslintConfig;
